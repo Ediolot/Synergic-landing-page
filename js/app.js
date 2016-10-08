0b95a5e49e09bb9fe9c9db71524ca34814217582
Jorge Sierra <jsierra.siete@gmail.com>
2016-10-08 14:44:27 +0100

    Paper plane

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+
+window.onload = function ()
+{
+    generateGitGraphImage();
+}
+
+
+function generateGitGraphImage()
+{
+/*    var gitgraph = new GitGraph({
+        template:    "metro",
+        orientation: "horizontal",
+        mode:        "compact"
+    });
+
+    var master = gitgraph.branch("master");
+    gitgraph.commit().commit().commit();
+    var develop = gitgraph.branch("develop");    // New branch from HEAD
+var myfeature = develop.branch("myfeature"); // New branch from develop
+develop.commit({
+  dotColor: "white",
+  dotSize: 10,
+  dotStrokeWidth: 10,
+});*/
+}
