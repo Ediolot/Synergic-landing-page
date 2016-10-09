f357eb470bd35a8ea5bfb08a462f7f5f6bce1dc5
Jorge Sierra <jsierra.siete@gmail.com>
2016-10-09 20:16:29 +0100

    End

--- js/app.js
+++ js/app.js
@@ -6,6 +6,13 @@ window.onload = function ()
     document.addEventListener("scroll", movePaperPlane);
     window.addEventListener("resize", movePaperPlane);
     document.getElementById("create-acc").addEventListener("click", submitInformation);
+
+    var form = document.getElementById("sing-up-form");
+    for (var i=0; i<form.children.length; ++i)
+    {
+        form.children[i].addEventListener("keyup", checkForm);
+        form.children[i].addEventListener("keydown", checkForm);
+    }
 }
 
 function movePaperPlane()
@@ -25,7 +32,7 @@ function movePaperPlane()
 
 function goToSingUp()
 {
-    const ERROR = 0.2;
+    var ERROR = 2;
 
     hiddeSingUpImage();
     movePaperPlane();
@@ -65,7 +72,73 @@ function displaySingUpImage()
     document.getElementById("create-acc").classList.add("hidden");
 }
 
+
+function checkForm()
+{
+    var NAME_REGEX  = /^[A-Z][a-z']+(\s[A-Z][a-z']+)+$/;
+    var EMAIL_REGEX = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    var PASS_REGEX  = /^.{6,18}$/;
+
+    var nameEl       = document.getElementById("name-text");
+    var error_nameEl = document.getElementById("name-error");
+
+    var emailEl       = document.getElementById("email-text");
+    var error_emailEl = document.getElementById("email-error");
+
+    var passEl       = document.getElementById("pass-text");
+    var error_passEl = document.getElementById("pass-error");
+
+    var pass_confirmEl       = document.getElementById("confirm-pass-text");
+    var error_pass_confirmEl = document.getElementById("confirm-pass-error");
+
+    var check = [];
+
+    check[0] = checkName(nameEl, error_nameEl, NAME_REGEX)
+    check[1] = checkSimple(emailEl, error_emailEl, EMAIL_REGEX)
+    check[2] = checkSimple(passEl, error_passEl, PASS_REGEX)
+    check[3] = checkPasswordConfirm(passEl, pass_confirmEl, error_pass_confirmEl);
+
+    return (check[0] && check[1] && check[2] && check[3]) && (nameEl!="" && emailEl!="" && passEl!="" && pass_confirmEl!="");
+}
+
+function checkName(textElement, errorElement, regex)
+{
+    var start = textElement.selectionStart;
+    var end   = textElement.selectionEnd;
+
+    var oldText = textElement.value;
+    var newText = textElement.value.replace(/ +/g, " ");
+    var valid   = regex.test(newText) || textElement.value=="";
+
+    if (textElement == document.activeElement)
+    {
+        textElement.value = newText;
+        textElement.setSelectionRange(start, oldText!=newText ? end-1 : end);
+        errorElement.style.visibility = valid ? "hidden" : "visible";
+    }
+
+    return valid;
+}
+
+function checkSimple(textElement, errorElement, regex)
+{
+    var valid = regex.test(textElement.value) || textElement.value=="";
+    errorElement.style.visibility = valid ? "hidden" : "visible";
+    return valid;
+}
+
+function checkPasswordConfirm(textElement_pass, textElement_confirm, errorElement)
+{
+    var valid = textElement_pass.value==textElement_confirm.value || textElement_confirm.value=="";
+    errorElement.style.visibility = valid ? "hidden" : "visible";
+    return valid;
+}
+
 function submitInformation()
 {
-    displaySingUpImage();
+    if (!checkForm()) return;
+
+    document.getElementById("sing-up-form").submit();
+    console.log(document.getElementById("sing-up-form"));
+    //displaySingUpImage();
 }
